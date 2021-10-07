78153be7ade7b68a60cd19ba9dfb83d2713e55bc
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-10-07 22:34:56 +0200

    add basic

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const delay = (ms) => {
+  // Твій код
+};
+
+const logger = (time) => console.log(`Resolved after ${time}ms`);
+
+// Виклич функції для перевірки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
