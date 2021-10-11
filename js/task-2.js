97c9bcbf4defe48b3ca522573c8268079d2981e1
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-10-11 20:10:33 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,19 @@
 const users = [
-  { name: "Mango", active: true },
-  { name: "Poly", active: false },
-  { name: "Ajax", active: true },
-  { name: "Lux", active: false },
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
 ];
 
 const toggleUserState = (allUsers, userName) => {
-  const updatedUsers = allUsers.map((user) =>
+  const updatedUsers = allUsers.map(user =>
     user.name === userName ? { ...user, active: !user.active } : user
   );
 
-  return new Promise((resolve) => resolve(updatedUsers));
+  return Promise.resolve(updatedUsers);
 };
 
-const logger = (updatedUsers) => console.table(updatedUsers);
+const logger = updatedUsers => console.table(updatedUsers);
 
-toggleUserState(users, "Mango").then(logger);
-toggleUserState(users, "Lux").then(logger);
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
